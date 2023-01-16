686e9b92b3580818db09e63e629e7d845b256299
paul <you@yourdomain.example.com>
2023-01-17 00:34:38 +0100

    metapask log-in added

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,8 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import logo from "../assets/youtube_logo.png";
 import SearchBar from "./SearchBar";
+import meta from "../assets/MetaMask_logo.png";
+import OpenApp from "react-open-app";
 
 const Header = ({ profile, searchResult }) => {
   const navigate = useNavigate();
@@ -26,21 +28,31 @@ const Header = ({ profile, searchResult }) => {
       <div>
         {profile ? (
           <div className="logged-in">
-            <button
+            {/* <button
               className="button-logout"
               onClick={() => {
                 navigate("/login");
               }}
             >
               Log out
-            </button>
-            <img classname="profile-image" src={profile.imageUrl} alt="user" />
+            </button> */}
+            <img
+              classname="profile-image"
+              src={meta}
+              alt="user"
+              style={{ height: "60px" }}
+              onClick={() => {
+                navigate("/login");
+              }}
+            />
+            {/* <img classname="profile-image" src={profile.imageUrl} alt="user" /> */}
+            {/* <p className="profile-name">{profile.enName}</p> */}
           </div>
         ) : (
           <button
             className="button-login"
             onClick={() => {
-              navigate("/login");
+              <OpenApp href="https://metamask.io/download/">MetaMask</OpenApp>;
             }}
           >
             Log in
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,53 +1,128 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { GoogleLogin, GoogleLogout } from "react-google-login";
+// import { GoogleLogin, GoogleLogout } from "react-google-login";
+import { ethers } from "ethers";
+import networks from "../assets/networks.json";
+import OpenApp from "react-open-app";
 
 const Login = ({ setProfile, profile, clientId }) => {
   const navigate = useNavigate();
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [defaultAccount, setDefaultAccount] = useState(null);
+  const [userBalance, setUserBalance] = useState(null);
+  const [network, setNetwork] = useState(null);
 
-  const onSuccess = (res) => {
-    setProfile(res.profileObj);
-    navigate("/");
+  const connectWallet = () => {
+    if (window.ethereum) {
+      window.ethereum
+        .request({ method: "eth_requestAccounts" })
+        .then((result) => {
+          accountChanged([result[0]]);
+          console.log(result[0]);
+        });
+    } else {
+      // setErrorMessage("Install Metamask");
+      // console.log({ errorMessage });
+      alert("install metamask extension!!");
+      return <OpenApp href="https://metamask.io/download/">MetaMask</OpenApp>;
+    }
   };
 
-  const onFailure = (err) => {
-    console.log("failed", err);
+  const accountChanged = (accountName) => {
+    setDefaultAccount(accountName);
+    getUserBalance(accountName);
+    getNetwork(accountName);
+    changeNetwork(accountName);
+    setProfile(accountName);
   };
 
-  const logOut = () => {
-    setProfile(null);
-    navigate("/");
+  const getUserBalance = (accountAddress) => {
+    window.ethereum
+      .request({
+        method: "eth_getBalance",
+        params: [String(accountAddress), "latest"],
+      })
+      .then((balance) => {
+        setUserBalance(ethers.utils.formatEther(balance));
+        console.log(balance);
+      });
   };
+
+  const getNetwork = (accountAddress) => {
+    window.ethereum
+      .request({ method: "net_version", params: [String(accountAddress)] })
+      .then((network) => {
+        setNetwork(network);
+        console.log(network);
+      });
+  };
+
+  const network_name = networks.map((element, i) => {
+    if (element.chainId.toString() === network) {
+      return <div>{element.name}</div>;
+    } else return null;
+  });
+
+  const changeNetwork = async ({ networkName }) => {
+    try {
+      await window.ethereum.request({
+        method: "wallet_switchEthereumChain",
+        params: [
+          {
+            chainId: "0x5",
+          },
+        ],
+      });
+    } catch (err) {
+      setErrorMessage(err.message);
+    }
+  };
+
+  const handleNetworkSwitch = async (networkName) => {
+    await changeNetwork({ networkName });
+  };
+
+  const networkChanged = (chainId) => {
+    console.log({ chainId });
+    connectWallet();
+  };
+
+  useEffect(() => {
+    window.ethereum.on("chainChanged", networkChanged);
+
+    return () => {
+      window.ethereum.removeListener("chainChanged", networkChanged);
+    };
+  }, []);
 
   return (
     <div className="login-container">
-      <h2>Login in and out with Google</h2>
+      <h2>Login in and out with Metamask</h2>
       <br />
       <br />
       {profile ? (
-        <div className="login-details">
-          <img src={profile.imageUrl} alt="user" />
-          <h3>User Logged in</h3>
-          <p>Name: {profile.name}</p>
-          <p>Email Address: {profile.email}</p>
+        <div>
+          <button className="button-logout meta" onClick={connectWallet}>
+            Connect wallet
+          </button>
+          <h3 style={{ marginBottom: "10px", fontWeight: "bold" }}>
+            User Logged in
+          </h3>
+          <h3 style={{ marginBottom: "10px" }}>Address: {defaultAccount}</h3>
+          <p style={{ marginBottom: "10px" }}>Balance: {userBalance}</p>
+          <p style={{ marginBottom: "10px" }}>Network: {network_name}</p>
           <br />
           <br />
-          <GoogleLogout
-            clientId={clientId}
-            buttonText="Log out"
-            onLogoutSuccess={logOut}
-          />
+          <button
+            className="button-logout meta"
+            onClick={() => handleNetworkSwitch()}
+          >
+            Switch to Goerli
+          </button>
         </div>
       ) : (
-        <GoogleLogin
-          className="login"
-          clientId={clientId}
-          buttonText="Sign in with Google"
-          onSuccess={onSuccess}
-          onFailure={onFailure}
-          cookiePolicy={"single_host_origin"}
-          isSignedIn={true}
-        />
+        <OpenApp href="https://metamask.io/download/">MetaMask</OpenApp>
+        // <button onClick={connectWallet}>Connect wallet</button>
       )}
     </div>
   );
